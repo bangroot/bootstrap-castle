1091756f065378e91db572f8b999d7ca6686eed0
David Brown <david_brown@mastercard.com>
2015-01-22 10:14:59 -0600

    enhancing bootstrapo

--- home/bin/bangstrap.sh
+++ home/bin/bangstrap.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 
-git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+if [-d $HOME/.homesick/repos/homeshick ]; then
+	git pull $HOME/.homesick/repos/homeshick
+else
+	git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+fi
+
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 export PATH=$PATH:~/bin
 curl -o ~/bin/mr https://raw.githubusercontent.com/bangroot/bootstrap-castle/master/home/bin/mr
-curl -o ~/.mrconfig https://raw.githubusercontent.com/bangroot/bootstrap-castle/master/home/.mrconfig
-chmod 600 ~/.mrconfig
 chmod 755 ~/bin/mr
+mr bootstrap https://raw.githubusercontent.com/bangroot/bootstrap-castle/master/home/.mrconfig
 
