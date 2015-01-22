7f7868e75149ce7dfc2cb279d89146ffcd245ff3
David Brown <david_brown@mastercard.com>
2015-01-22 10:20:07 -0600

    bootstrap work

--- home/bin/bangstrap.sh
+++ home/bin/bangstrap.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 
-if [-d $HOME/.homesick/repos/homeshick ]; then
+if [ -d $HOME/.homesick/repos/homeshick ]; then
 	git pull $HOME/.homesick/repos/homeshick
 else
 	git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 fi
 
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+if [ ~ -e $HOME/bin ]; then
+	mkdir $HOME/bin
+fi
+
 export PATH=$PATH:~/bin
 curl -o ~/bin/mr https://raw.githubusercontent.com/bangroot/bootstrap-castle/master/home/bin/mr
 chmod 755 ~/bin/mr
